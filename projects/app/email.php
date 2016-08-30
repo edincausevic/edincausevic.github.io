f7af10674671a863b6fb071761cde4f6f3b68606
Edin Causevic <causevicedin@live.com>
2016-08-30 17:40:04 +0200

    Hofer

--- projects/app/email.php
+++ projects/app/email.php
@@ -1,13 +1,39 @@
-<?PHP
+<?php
 
-$myEmailAddress = "causevicedinbih@gmail.com";
+session_start(); 
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$text = $_POST["text"];
-$header = "from: $name <$email>";
+	$name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["text"];
+    $subject = $_POST["subject"];
+    $EmailTo = "mrcausevic@gmail.com";
+    $EmailFrom = "contact@zahnhofer.at";
+    
+    // prepare email body text
+	$Body = "";
+	$Body .= "Name: ";
+	$Body .= $name;
+	$Body .= "\n";
+	$Body .= "Email: ";
+	$Body .= $email;
+	$Body .= "\n";
+	$Body .= "Message: ";
+	$Body .= $message;
+	$Body .= "\n";
+	
+	
+	// send email
+	$success = mail($EmailTo, $subject, $Body, "From:".$EmailFrom);
+	unset ($EmailTo);
+	// redirect to success page
+	
+	if ($success){
+   		$_SESSION['msg'] = '<p>Poslao</p>';
+   		
+	}else{
+        $_SESSION['msg'] = '<p>GreskAAAAAA</p>';
+	}
+	
 
-mail($myEmailAddress, $subject, $text, $header);
-
+header("Location: " . $_SERVER["HTTP_REFERER"]);
 ?>
--- projects/app/index.php
+++ projects/app/index.php
@@ -1,3 +1,7 @@
+<?php 
+session_start();
+$msg = $_SESSION['msg'];
+?>
 <!DOCTYPE html>
 <html lang="de">
     <head>
@@ -171,24 +175,30 @@
             <div class="row">
                 <h2>Wir sind glücklich, VON IHNEN ZU HÖREN</h2>
             </div>
+            
+            <!-- POruka od php-a -->
+            <?php echo $msg; ?>
+            <!-- POruka od php-a -->
+            
+            
             <div class="row con-left">
                 <div id="animate-form">
                     <div class="msg-container">
                         <form method="POST" action="email.php" id="email-form">
                             <div class="msg-left">
                                 <label for="name">Dein Name*</label>
-                                <input type="text" id="name" autocomplete="off" />
+                                <input type="text" id="name" name="name" autocomplete="off" />
                                 <p id="name-error">&nbsp;</p>
                                 <label for="email">Deine E-Mail*</label>
-                                <input type="text" id="email" autocomplete="off" />
+                                <input type="text" id="email" name="email" autocomplete="off" />
                                 <p id="email-error">&nbsp;</p>
                                 <label for="subject">Dein Fach*</label>
-                                <input type="text" id="subject" autocomplete="off" />
+                                <input type="text" id="subject" name="subject" autocomplete="off" />
                                 <p id="subject-error">&nbsp;</p>
                             </div>
                             <div class="msg-right">
                                 <label>Nachricht Senden*</label>
-                                <textarea></textarea>
+                                <textarea name="text"></textarea>
                                 <p id="text-error">&nbsp;</p>
                                 <input type="submit" value="Senden sie ihre nachricht" id="submit">
                             </div>

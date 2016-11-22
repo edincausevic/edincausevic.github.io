9ee8477c8f4ef4f83553f9f4f1ae791908c4f718
Edin Causevic <causevicedin@live.com>
2016-11-22 19:52:45 +0100

    Hofer

--- projects/app/aktuelles.php
+++ projects/app/aktuelles.php
@@ -56,10 +56,11 @@
     <div class="row">
         <div class="aktuelles">
             <ul>
+                
                 <li>
                     <div class="note-left">
                         <img src="img/prifile-photo.png" alt="Hofer Image"/>
-                        <span class="akt-profile-name">DR Critoph Hofer</span>
+                        <span class="akt-profile-name">Dr Critoph Hofer</span>
                         <span class="akt-date">22/03/2005</span>
                     </div>
                     <div class="note-right">
@@ -67,6 +68,7 @@
                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                     </div>
                 </li>
+
             </ul>
         </div>
     </div>
